81f34f5515fadf4bf11df46bf5c5aef787ff431f
muelsyse <2402686765@qq.com>
2026-09-10 10:52:21 +0800

    lean: disambiguate arbitrary-order decay exponent sign

--- formal/AMLStabilization/GeneralPolynomialEnergyDecay.lean
+++ formal/AMLStabilization/GeneralPolynomialEnergyDecay.lean
@@ -92,8 +92,9 @@ theorem energy_le_rpow_of_superquadratic_dissipation
   have hA : 0 < A := by
     dsimp [A]
     linarith
+  have htwo : (0 : ℝ) ≤ 2 := by norm_num
   have hz : -2 / theta ≤ 0 :=
-    neg_nonpos.mpr (div_nonneg (by norm_num) (le_of_lt htheta))
+    neg_nonpos.mpr (div_nonneg htwo (le_of_lt htheta))
   have hrpow := Real.rpow_le_rpow_of_nonpos hA hlow hz
   have htheta0 : theta ≠ 0 := ne_of_gt htheta
   have hexp : (-theta / 2) * (-2 / theta) = 1 := by
