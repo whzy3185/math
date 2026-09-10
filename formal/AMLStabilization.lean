53c7e06ca4b54cde8f8ed5a262a04cb5837db3c2
muelsyse <2402686765@qq.com>
2026-09-10 16:07:11 +0800

    lean: add Poincare tensorization core to verification root

--- formal/AMLStabilization.lean
+++ formal/AMLStabilization.lean
@@ -8,6 +8,7 @@ import AMLStabilization.HolderCore
 import AMLStabilization.HolderCoercivityBridge
 import AMLStabilization.PoincareMeanCore
 import AMLStabilization.BoxPoincareCore
+import AMLStabilization.BoxPoincareTensorCore
 import AMLStabilization.InvariantRangeCore
 import AMLStabilization.SignalEnergyIdentityCore
 import AMLStabilization.FinalSignalAssembly
