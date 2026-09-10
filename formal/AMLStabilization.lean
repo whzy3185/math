d3c3af42964592d0a0eed9abf9653b78ce57cd11
muelsyse <2402686765@qq.com>
2026-09-10 15:46:49 +0800

    lean: add product-rule Green core to verification root

--- formal/AMLStabilization.lean
+++ formal/AMLStabilization.lean
@@ -17,6 +17,7 @@ import AMLStabilization.LpRateOptimization
 import AMLStabilization.MassConservationCore
 import AMLStabilization.EnergyDifferentiationCore
 import AMLStabilization.BoxNeumannFluxCore
+import AMLStabilization.ProductRuleGreenCore
 import AMLStabilization.GeneralPolynomialEnergyDecay
 import AMLStabilization.NonlinearCoercivityAlgebra
 import AMLStabilization.WeightedHolderQ
