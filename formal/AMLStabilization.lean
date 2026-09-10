01ca385a92a797ed22852551a3b423f31006680a
muelsyse <2402686765@qq.com>
2026-09-10 18:30:24 +0800

    lean: add product variance identity to verification root

--- formal/AMLStabilization.lean
+++ formal/AMLStabilization.lean
@@ -10,6 +10,7 @@ import AMLStabilization.PoincareMeanCore
 import AMLStabilization.BoxPoincareCore
 import AMLStabilization.BoxPoincareTensorCore
 import AMLStabilization.BoxPoincareCoercivityBridge
+import AMLStabilization.ProductVarianceCore
 import AMLStabilization.InvariantRangeCore
 import AMLStabilization.MaximumPrincipleContactCore
 import AMLStabilization.SpatialExtremumSecondDerivativeCore
