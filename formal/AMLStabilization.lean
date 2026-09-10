522198d418ade62fecbc38380ec4abc11359eebd
muelsyse <2402686765@qq.com>
2026-09-10 18:57:58 +0800

    lean: add integrated coordinate telescoping to verification root

--- formal/AMLStabilization.lean
+++ formal/AMLStabilization.lean
@@ -14,6 +14,7 @@ import AMLStabilization.BoxPoincareTensorCore
 import AMLStabilization.BoxPoincareCoercivityBridge
 import AMLStabilization.ProductVarianceCore
 import AMLStabilization.CoordinateTelescopingCore
+import AMLStabilization.IntegratedCoordinateTelescopingCore
 import AMLStabilization.BoxProductMeasureCore
 import AMLStabilization.PairedProductMeasureCore
 import AMLStabilization.InvariantRangeCore
