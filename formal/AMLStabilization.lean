3d63bb228f24453cea0ddda49d6ff23295e85986
muelsyse <2402686765@qq.com>
2026-09-11 17:16:53 +0800

    lean: add box Poincare cell interface to root

--- formal/AMLStabilization.lean
+++ formal/AMLStabilization.lean
@@ -25,6 +25,7 @@ import AMLStabilization.PiCoordinateFubiniCore
 import AMLStabilization.BoxHybridFiberPoincareCore
 import AMLStabilization.FullBoxPoincareCore
 import AMLStabilization.FullBoxPoincareSqrtCore
+import AMLStabilization.BoxPoincareCellInterfaceCore
 import AMLStabilization.FullBoxCoercivityBridge
 import AMLStabilization.InvariantRangeCore
 import AMLStabilization.MaximumPrincipleContactCore
