44c00d9d40b8a22300e6c8a953b7edac7e1b361c
muelsyse <2402686765@qq.com>
2026-09-11 14:17:50 +0800

    lean: add motility chain rule to verification root

--- formal/AMLStabilization.lean
+++ formal/AMLStabilization.lean
@@ -55,6 +55,7 @@ import AMLStabilization.CrossBoxGreenCore
 import AMLStabilization.FiniteGradientCauchyCore
 import AMLStabilization.BoxCellEnergyPDECore
 import AMLStabilization.TimeDependentBoxCellEnergyCore
+import AMLStabilization.MotilityChainRuleCore
 import AMLStabilization.GeneralPolynomialEnergyDecay
 import AMLStabilization.NonlinearCoercivityAlgebra
 import AMLStabilization.WeightedHolderQ
